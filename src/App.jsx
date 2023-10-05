94a888d8e500e4a30df9c9a04f75e7275bc22005
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-10-05 14:08:53 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='navbar mx-auto flex p-4'>
+      <h1 className='text-4xl font-bold text-blue-500'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
