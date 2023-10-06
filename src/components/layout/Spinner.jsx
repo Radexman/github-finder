666776e15adf1ed2037bb4766ac103b9f422bd1e
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-10-06 11:37:16 +0200

    Spinner Component

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className='flex items-center justify-center'>
+      <span className='loading loading-spinner loading-lg'></span>
+    </div>
+  );
+};
+
+export default Spinner;
