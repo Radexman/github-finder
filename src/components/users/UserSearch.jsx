7eccbdb6653b53c83c0ff944befd702965ecf054
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-10-11 17:18:35 +0200

    Alert Component

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,8 +1,11 @@
 import { useState, useContext } from 'react';
 import GithubContext from '../../context/github/GithubContext';
+import AlertContext from '../../context/alert/AlertContext';
+import Alert from '../layout/Alert';
 
 const UserSearch = () => {
   const { searchUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
   const [text, setText] = useState('');
 
   const handleChange = (e) => {
@@ -13,7 +16,7 @@ const UserSearch = () => {
     e.preventDefault();
 
     if (text === '') {
-      window.alert('Please enter somthing');
+      setAlert('Please enter something', 'error');
     } else {
       searchUsers(text);
     }
@@ -21,29 +24,32 @@ const UserSearch = () => {
   };
 
   return (
-    <div className='mb-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2'>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div className='form-control'>
-            <div className='relative'>
-              <input
-                type='text'
-                className='input input-lg w-full bg-gray-200 pr-40 text-black'
-                placeholder='Search'
-                value={text}
-                onChange={handleChange}
-              />
-              <button
-                type='submit'
-                className='btn btn-primary btn-lg absolute right-0 top-0 w-36 rounded-l-none'
-              >
-                Go
-              </button>
+    <>
+      <div className='mb-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2'>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <div className='form-control'>
+              <div className='relative'>
+                <input
+                  type='text'
+                  className='input input-lg w-full bg-gray-200 pr-40 text-black'
+                  placeholder='Search'
+                  value={text}
+                  onChange={handleChange}
+                />
+                <button
+                  type='submit'
+                  className='btn btn-primary btn-lg absolute right-0 top-0 w-36 rounded-l-none'
+                >
+                  Go
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
+      <Alert />
+    </>
   );
 };
 
