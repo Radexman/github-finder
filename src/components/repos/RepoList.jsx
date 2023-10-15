b2a29ec924f440b3132722745f444d3e8b331783
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-10-15 14:56:01 +0200

    Repo Items

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types';
+import RepoItem from './RepoItem';
 
 const RepoList = ({ repos }) => {
   return (
     <div className='card rounded-lg bg-base-100 shadow-lg'>
       <div className='card-body'>
-        <h2 className='card-title mx-auto my-4 text-3xl font-bold'>
+        <h2 className='card-title my-4 -ml-6 text-3xl font-bold'>
           Latest Repositories
         </h2>
       </div>
-      {repos.map((repo, index) => (
-        <h3 key={index}>{repo.name}</h3>
+      {repos.map((repo) => (
+        <RepoItem key={repo.id} repo={repo} />
       ))}
     </div>
   );
